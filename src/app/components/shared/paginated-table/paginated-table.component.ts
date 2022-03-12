fee8f139924fe91d51e1af5a7b26a6ba49e99ded
Fede-YaoPai <federico.lecca@hotmail.com>
2022-03-12 01:57:27 +0100

    Added dynamic paginator items slide when total items exceed max displayed items

--- src/app/components/shared/paginated-table/paginated-table.component.ts
+++ src/app/components/shared/paginated-table/paginated-table.component.ts
@@ -15,15 +15,14 @@ export class PaginatedTableComponent implements OnInit {
   @Input() dataSource!: any[];
   @Input() displayedColumns!: string[];
   @Input() displayedProps!: string[];
-  @Input() itemsPerPage!: number;
-  @Input() maxDisplayedPages: number = 10;
+  @Input() tableClasses!: string[];
+  @Input() rowsPerPage!: number;
   @Input() showIndex: boolean = false;
 
-  @Input() tableClasses!: string[];
+  @Input() maxDisplayedPaginatorItems: number = 10;
   @Input() paginatorClasses!: string[];
   @Input() paginatorItemClasses!: string[];
   @Input() paginatorLinkClasses!: string[];
-
   @Input() firstPageInnerHTML!: string;
   @Input() previousPageInnerHTML!: string;
   @Input() nextPageInnerHTML!: string;
@@ -32,6 +31,7 @@ export class PaginatedTableComponent implements OnInit {
   public currentPage: number = 1;
   public totalPages: number = 0;
   public maxPagesExceeded: boolean = false;
+  public paginatorSlideFactor: number = 1;
 
   public readonly paginatorClass: string = 'paginator-fyp';
   public readonly paginatorItemClass: string = 'paginator-item-fyp';
@@ -49,7 +49,7 @@ export class PaginatedTableComponent implements OnInit {
     if (!displayedPropsValidity.valid) {
       throw new Error(
         `PaginatedTableError: displayedProps items [${displayedPropsValidity.errors}] do not match the table's dataSource object structure!`
-      )
+      );
     }
 
     let filteredDataSource: any[] = [];
@@ -86,41 +86,59 @@ export class PaginatedTableComponent implements OnInit {
   }
 
   private getTotalPages(array: any[]): number {
-    let totalPages: number = Math.ceil(array.length / this.itemsPerPage);;
+    let totalPages: number = Math.ceil(array.length / this.rowsPerPage);;
 
-    if (totalPages <= this.maxDisplayedPages) {
+    if (totalPages <= this.maxDisplayedPaginatorItems) {
       this.maxPagesExceeded = false;
     }
     else {
       this.maxPagesExceeded = true;
-      totalPages = this.maxDisplayedPages - 1;
     }
 
     return totalPages;
   }
 
   private getSliceStart(): number {
-    return this.itemsPerPage * (this.currentPage - 1);
+    return this.rowsPerPage * (this.currentPage - 1);
   }
 
   private getSliceEnd(): number {
-    return this.getSliceStart() + this.itemsPerPage;
+    return this.getSliceStart() + this.rowsPerPage;
   }
 
   public firstPage(): void {
     if (!this.isFirstPage()) this.currentPage = 1;
+
+    if (this.paginatorSlideFactor > 1) {
+      this.paginatorSlideFactor = 1;
+    }
   }
 
   public previousPage(): void {
     if (!this.isFirstPage()) this.currentPage--;
+
+    if (this.paginatorSlideFactor > 1) {
+      if (this.currentPage != this.totalPages - 1)
+        this.paginatorSlideFactor--;
+    }
   }
 
   public nextPage(): void {
     if (!this.isLastPage()) this.currentPage++;
+
+    if (this.maxPagesExceeded) {
+      if (this.currentPage > this.getPagesArray().length && this.paginatorSlideFactor < this.totalPages - this.getPagesArray().length) {
+        this.paginatorSlideFactor++;
+      }
+    }
   }
 
   public lastPage(): void {
     if (!this.isLastPage()) this.currentPage = this.totalPages;
+
+    if (this.maxPagesExceeded) {
+      this.paginatorSlideFactor = this.totalPages - this.getPagesArray().length;
+    }
   }
 
   public thisPage(page: number): void {
@@ -151,9 +169,15 @@ export class PaginatedTableComponent implements OnInit {
 
   public getShownIndex(i: number): number {
     let previousPages: number = this.currentPage - 1;
-    let shownIndex: number = (previousPages * this.itemsPerPage) + i;
+    let shownIndex: number = (previousPages * this.rowsPerPage) + i;
 
     return shownIndex;
+  }
+
+  public getPagesArray() {
+    let arrayConstructor: number = this.maxPagesExceeded ? this.maxDisplayedPaginatorItems - 2 : this.totalPages;
+
+    return [].constructor(arrayConstructor);
   }
 
 }
