1c2fe86077e2d6dc1bc5386c3f25ed747ad5a03d
Fede-YaoPai <federico.lecca@hotmail.com>
2022-03-13 22:03:21 +0100

    Installed library, removed component

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,21 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './features/home/home.component';
 import { InfoComponent } from './features/info/info.component';
-import { PaginatedTableComponent } from './shared/paginated-table/paginated-table.component';
+import { AngularPaginatedTableFypModule } from 'angular-paginated-table-fyp';
 
 
 
 @NgModule({
   declarations: [
     HomeComponent,
-    InfoComponent,
-    PaginatedTableComponent
+    InfoComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    AngularPaginatedTableFypModule
   ],
   exports: [
-    PaginatedTableComponent
   ]
 })
 export class ComponentsModule { }
